f220004cde71fb0d24f1a07be32719f93579a0e6
hungmike <maingochungnuce@gmail.com>
2023-11-11 09:47:44 +0700

    complete home page

---
+++ js/app.js
@@ -0,0 +1,17 @@
+$(".customer-staff-list").slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows: false,
+  dots: true,
+});
+
+$(".solution-list").slick({
+  infinite: true,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  arrows: false,
+  dots: true,
+  centerMode: true,
+  centerPadding: "150px",
+});
